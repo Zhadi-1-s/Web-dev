f77cd6e83032c7a9c515b4f5adff92adfa1c40b8
Zhadi-1-s <98839309+Zhadi-1-s@users.noreply.github.com>
2023-02-22 23:09:39 +0600

    Add files via upload

---
+++ src/app/products.ts
@@ -0,0 +1,80 @@
+export interface Product {
+  id: number;
+  name: string;
+  price: number;
+  description: string;
+}
+
+export const products = [
+  {
+    id: 1,
+    name: 'Geforce GTX 1660',
+    price: 5000,
+    description: 'The best ',
+  },
+  {
+    id: 2,
+    name: 'Gefroce GTX 1660 TI',
+    price: 5300,
+    description:
+      'Улучшенная версия видеокарты gtx 1660, производительность и охлаждение на другом уровне',
+  },
+  {
+    id: 3,
+    name: 'Geforce RTX 2090',
+    price: 6500,
+    description:
+      'Здесь внедрены настройки ультра которая позволяет быстро генерировать чанки в майнкрафте',
+  },
+  {
+    id: 4,
+    name: 'Geforce GTX 1050',
+    price: 2000,
+    description:
+      'На нем можно свободно играть в такие игры как гта сан андреас,контр страйк1.6',
+  },
+  {
+    id: 5,
+    name: 'Geforce GTX 2060',
+    price: 5800,
+    description:
+      'Если вы хотите играть в кс го на максималках то советую брать это а не 3090',
+  },
+  {
+    id: 6,
+    name: 'Geforce GTX 1050 TI',
+    price: 2100,
+    description: 'Тянет гта 4 на минималках но не больше',
+  },
+  {
+    id: 7,
+    name: 'Geforce GTX 4070',
+    price: 8000,
+    description:
+      'Ну тут даже слова не нужны скачаешь шейдеры в майнкрафт на ультрах и играешь на нем гта 5',
+  },
+  {
+    id: 8,
+    name: 'Gefroce GT 710',
+    price: 1000,
+    desccription: 'смотришь фильмы на хорошем качестве',
+  },
+  {
+    id: 9,
+    name: 'Gefroce GTX 1650 Gaming Pro 4GB',
+    price: 5000,
+    description: 'теперь и дота не лагает',
+  },
+  {
+    id: 10,
+    name: 'Gefroce RTX 4070 TI',
+    price: 8300,
+    description: 'Та же самая видюха но с добавлением крутого окончания TI',
+  },
+];
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
